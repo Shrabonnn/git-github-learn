682d72d4342a793ee7bc35af438bc8f28a552a74
shrabonnn <shrabonewmc009@gmail.com>
2026-04-28 11:41:36 +0600

    school created

---
+++ lib/school.dart
@@ -0,0 +1,17 @@
+// school created
+import 'package:flutter/material.dart';
+
+class School extends StatelessWidget {
+  const School({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Text("School created")
+        ],
+      ),
+    );
+  }
+}
